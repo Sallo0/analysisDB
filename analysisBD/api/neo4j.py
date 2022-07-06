f1720f0fbf3bdc4be309af4a404436c21cb23737
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-07-06 13:47:40 +0500

    working skelet

---
+++ analysisBD/api/neo4j.py
@@ -0,0 +1,5 @@
+def getDataNeo4j(request):
+
+    #do somethink
+
+    return {1: "тест1", 2: "тест2"} #Это обязательно JSON
--- analysisBD/api/views.py
+++ analysisBD/api/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from . import neo4j
 
 def hello(request):
     return render(request, 'index.html')
+
+
+def orient(request):
+    return render(request, 'orientdb.html')
+
+
+def postgre(request):
+    return render(request, 'postgresql.html')
+
+
+def Neo4j(request):
+    return render(request, 'neo4j.html')
+
+
+@api_view(['GET'])
+def getNeo4jData(request):
+    return Response(data=neo4j.getDataNeo4j(request), status=200)
