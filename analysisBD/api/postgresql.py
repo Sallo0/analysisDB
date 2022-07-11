0d9b081ceb84a9046e16f99c7cf3883897b549f3
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-07-11 12:09:46 +0500

    try to connect postgres

--- analysisBD/api/postgresql.py
+++ analysisBD/api/postgresql.py
@@ -7,7 +7,7 @@ def getDataPostgreSQL(request):
     host = "127.0.0.1"
     user = "postgres"
     password = "postgres"
-    db_name = "test"
+    db_name = "postgres"
     try:
         connection = psycopg2.connect(
             host=host,
@@ -16,15 +16,15 @@ def getDataPostgreSQL(request):
             database=db_name
             )
         with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor.execute("SELECT * FROM test WHERE LIMIT 10")
+            cursor.execute("SELECT * FROM links WHERE LIMIT 10")
             result = cursor.fetchall()
             for record in result:
                 print(record)
                 print(result)
             to_json = json.dumps(result)
             print(to_json)
-    except Exception as _ex:
-        print("Error : ", _ex)
+    #except Exception as _ex:
+        #print("Error : ", _ex)
     finally:
         if connection:
             connection.close()
