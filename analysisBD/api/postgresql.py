55115eb825bb0caae0f5f9d2b2c8b9bf1bb82654
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-07-12 10:50:18 +0500

    add timer postgres

--- analysisBD/api/postgresql.py
+++ analysisBD/api/postgresql.py
@@ -1,7 +1,11 @@
 import psycopg2
 import json
+import time
 from psycopg2.extras import RealDictCursor
 from django.core.serializers.json import DjangoJSONEncoder
+
+t = time
+timer = 0
 
 
 def getDataPostgreSQL(request):
@@ -18,9 +22,11 @@ def getDataPostgreSQL(request):
             database=db_name
             )
         with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            time_start = t.perf_counter()
             cursor.execute("SELECT * FROM links LIMIT 10")
+            time_end = t.perf_counter()
             result = cursor.fetchall()
-            print(result)
+            timer = time_end - time_start
             to_json = json.dumps(
                 result,
                 cls=DjangoJSONEncoder)
@@ -30,4 +36,7 @@ def getDataPostgreSQL(request):
         if connection:
             connection.close()
             print("connection closed")
+
+    to_json.append(timer)
+
     return to_json
