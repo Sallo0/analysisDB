f3a2ffc323b13456fac95425cf58472b1f4e91fe
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-07-20 12:50:42 +0500

    Перенос данных в neo4j

--- analysisBD/api/csv_script.py
+++ analysisBD/api/csv_script.py
@@ -1,4 +1,4 @@
-import psycopg2
+mport psycopg2
 import json
 import time
 import os
@@ -10,63 +10,57 @@ from django.core.serializers.json import DjangoJSONEncoder
 time_obj = time
 timer = 0.0
 
+connection_Neo = GraphDatabase.driver(
+        "bolt://localhost:5332",
+        auth=basic_auth("neo4j", "12345678"))
+
+driver = connection_Neo
+
 """
 connection = psycopg2.connect(
-            host="46.48.3.74",
-            #port="5432",
+            host="127.0.0.1", 
+            port="5432",
             user='postgres',
             password='postgres',
             database='postgres'
             )
 
-
 with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-    sql = "COPY (Select * from face_info order by face_id LIMIT 20000) TO STDOUT WITH CSV HEADER"
+    sql = "COPY (Select * from face_info) TO STDOUT WITH CSV HEADER"
 
-    with open("C:/Users/Nikita/.Neo4jDesktop/relate-data/dbmss/dbms-fb0b047d-5797-4e55-a723-267203e15904/import/faces.csv", "w") as file:
+    with open("/var/lib/neo4j/import/faces.csv", "w") as file:
         cursor.copy_expert(sql, file)
 
 cursor.close()
 
 with connection.cursor(cursor_factory=RealDictCursor) as cursor:
-    sql = "COPY (Select * from links order by parent LIMIT 20000) TO STDOUT WITH CSV HEADER"
+    sql = "COPY (Select * from links) TO STDOUT WITH CSV HEADER"
 
-    with open("C:/Users/Nikita/.Neo4jDesktop/relate-data/dbmss/dbms-fb0b047d-5797-4e55-a723-267203e15904/import/links.csv", "w") as file:
+    with open("/var/lib/neo4j/import/links.csv", "w") as file:
         cursor.copy_expert(sql, file)
 
 cursor.close()
-connection.close()
 """
-
-"""
-connection_Neo = GraphDatabase.driver(
-        "bolt://localhost:7687",
-        auth=basic_auth("neo4j", "123"))
-"""
-connection_Neo = GraphDatabase.driver(
-        "bolt://46.48.3.74:5332",
-        auth=basic_auth("neo4j", "12345678"))
-
-driver = connection_Neo
 
 with connection_Neo.session(database="neo4j") as session:
     time_start = time_obj.perf_counter()
 
-    #query_create_pk = 'create constraint exemplar_pk for (n:Exemplar) require n.pk is unique;'
+    #query_create_pk = 'CREATE CONSTRAINT exemplar_pk ON (n:Exemplar) ASSERT  n.pk IS UNIQUE;'
     #session.run(query_create_pk)
 
-    #query_create_nodes = "USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///faces.csv' as line CREATE (:Exemplar {pk:line.face_id, face_type:line.face_type, face_name:coalesce(line.face_name, '-')});"
+    #query_create_nodes = "USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///faces.csv' as line CREATE (:Exemplar {pk:line.face_id, face_type:line.fac>
     #session.run(query_create_nodes)
 
-    #query_create_links = "USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///links.csv' as line MATCH (p:Exemplar {pk: line.parent}), (c:Exemplar {pk: line.child}) MERGE (p)-[:Properties{kind:line.kind, date_begin:line.date_begin, date_end:coalesce(line.date_end, '-'),cost:coalesce(line.cost, '-'),share:coalesce(line.share, '-'), child_liquidated:line.child_liquidated}]->(c);"
-    #session.run(query_create_links)
+    query_create_links = "USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///links2.csv' as line MATCH (p:Exemplar {pk: line.parent}), (c:Exemplar {pk:r {pk: line.child}) MERGE (p)-[:Properties{kind:line.kind, date_begin:line.date_begin, date_end:coalesce(line.date_end, '-'),cost:coalesce(line.cost, '-'),share:coare:coalesce(line.share, '-'), child_liquidated:coalesce(line.child_liquidated, '-')}]->(c);"
 
-    query_create_links = "MATCH (p{pk: '15314139'})-[l]->(c{pk: '10398095'}) RETURN l LIMIT 10"
-    res = session.run(query_create_links)
-    print(res.data())
 
+    session.run(query_create_links)
 
     time_end = time_obj.perf_counter()
+
+"""
+connection.close()
+"""
 connection_Neo.close()
 print(time_end - time_start)
 
