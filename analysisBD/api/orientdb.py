32ba1b2d1af3a649f5f53ca438c76c96d8dace21
batyadmx <andronovaleksandrptg@yandex.ru>
2022-07-25 19:20:52 +0500

    fix flat list

--- analysisBD/api/orientdb.py
+++ analysisBD/api/orientdb.py
@@ -1,16 +1,14 @@
 import pyorient as po
 import json
-import analysisBD.settings as config
+import os
 
 class OrientDBRepository():
 
-    client = None
-
     def __init__(self):
-        self.client = po.OrientDB(config.ORIENTDB_IP, 2424)
+        self.client = po.OrientDB(os.getenv('orientdb_ip'), 2424)
         self.client.set_session_token( True )
-        self.client.connect(config.ORIENTDB_LOGIN, config.ORIENTDB_PASS)
-        self.client.db_open("dbforanal", config.ORIENTDB_LOGIN, config.ORIENTDB_PASS)
+        self.client.connect(os.getenv('orientdb_login'), os.getenv('orientdb_pass'))
+        self.client.db_open("dbforanal", os.getenv('orientdb_login'), os.getenv('orientdb_pass'))
 
     def query(self, query):
         a =self.client.query(query)
@@ -31,44 +29,50 @@ class OrientDBRepository():
         return parentsDict
 
     def flat_list(self, filter_data):
-        return list(map(lambda x: x.oRecordData, self.client.query(queryConstructor(filter_data))))
+        query = queryConstructor(filter_data)
+        return list(map(lambda x: x.oRecordData, self.client.query(query)))
 
 
 def queryConstructor(data):
-        """Создаёт sql запрос в виде строки по полученным фильтрам с сайта
+    """Создаёт sql запрос в виде строки по полученным фильтрам с сайта
 
-        Args:
-            data (dict): Json словарь полученый с фронта.
-        Returns:
-            str: Готовый SQL запрос.
-        """
-        face_as_json = "{{'id': {0}.id, 'name': {0}}.name, 'type': {0}.face_type}}"
-        query = [
-            f"select {face_as_json.format('in')},{face_as_json.format('out')},kind,date_begin,date_end,cost,share,child_liquidated from Link where "]
-        sql_args = []
+    Args:
+        data (dict): Json словарь полученый с фронта.
+    Returns:
+        str: Готовый SQL запрос.
+    """
+    face_as_json = "{{id: {0}.id, name: {0}.name, type: {0}.face_type}}"
+    query = [
+        f"select {face_as_json.format('out')},kind,date_begin,date_end,cost,share,child_liquidated from Link where "
+        #f"select out.name as name,out.id as id,out.face_type as type,kind,date_begin,date_end,cost,share,child_liquidated from Link where "
+    ]
+    sql_args = []
 
-        for k, v in data:
-            if v == "" or v == "-":
-                continue
-            if k == "date_begin":
-                sql_args.append(f"{k}>={v}")
-            if k == "date_end":
-                sql_args.append(f"{k}<={v}")
-            if k == "mainfilter":
-                if v['Parent'] != "":
-                    sql_args.append(f"out.id={v['Parent']})")
-                if v['Child'] != "":
-                    sql_args.append(f"in.id={v['Child']})")
+    for k, v in data.items():
+        if v == "" or v == "-":
+            continue
+        elif k == "date_begin":
+            sql_args.append(f"{k}>=date('{v}', 'yyyy-MM-dd')")
+        elif k == "date_end":
+            sql_args.append(f"{k}<=date('{v}', 'yyyy-MM-dd')")
+        elif k == "mainfilter":
+            if v['Parent'] != "":
+                sql_args.append(f"out.id={v['Parent']}")
+            if v['Child'] != "":
+                sql_args.append(f"in.id={v['Child']}")
+        else:
             sql_args.append(f"{k}={v}")
 
-        if len(data) == 0:
-            sql_args.append(f'pk=0')
+    if len(data) == 0:
+        sql_args.append(f'pk=0')
 
-        query.append(" AND ".join(sql_args))
+    query.append(" AND ".join(sql_args))
 
-        return "".join(query)
+    return "".join(query)
 
 
 def getDataOrientDB(request):
-    return OrientDBRepository().query("select name, out('Owning').name from Organization where name != 'EvilOrg' limit 25")
+    data = request.data
+    data.pop("dbtype")
+    return OrientDBRepository().flat_list(request.data)
 
