68f510385e4fd37de742d8b74bbc134f5fdbb066
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-07-27 14:02:28 +0500

    работаем над коленным поиском neo

--- analysisBD/api/neo4jDB.py
+++ analysisBD/api/neo4jDB.py
@@ -47,7 +47,7 @@ def queryConstructor(data):
         query.append(data['mainfilter']['Parent'])
         query.append("'})-[r:Properties{")
         query.append(",".join(filters))
-        query.append("}]->(c:Exemplar) return PROPERTIES(r), c ORDER BY c.pk")
+        query.append("}]->(c:Exemplar) return PROPERTIES(r), c")
 
     return "".join(query)
 
